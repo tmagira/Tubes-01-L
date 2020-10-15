6384345cb7dca6437128b7d5775d47b524a10397
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2020-10-15 12:43:59 +0700

    Add Main Fragment and Menu Fragment (Finished)

--- app/src/main/java/com/example/tubes_01_l/MainFragment.java
+++ app/src/main/java/com/example/tubes_01_l/MainFragment.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener{
     private FragmentManager fragmentManager;
     private FragmentListener listener;
+    private Button btnCari;
 
     public MainFragment(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_main,container, false);
+
+        this.btnCari = view.findViewById(R.id.btn_cari);
+
+        this.btnCari.setOnClickListener(this);
 
         return view;
     }
@@ -34,9 +40,13 @@ public class MainFragment extends Fragment {
 
     public static MainFragment newInstance(){
         MainFragment fragment = new MainFragment();
-//        Bundle args = new Bundle();
-//        args.putString("title", title);
-//        fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==this.btnCari){
+            listener.changePage(2);
+        }
     }
 }
