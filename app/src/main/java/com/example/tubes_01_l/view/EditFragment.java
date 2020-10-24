b0db0fef031a2f3a0b28cc9ce14b19efd41e3f4d
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2020-10-25 02:27:59 +0700

    Tinggal OnClickEdit

--- app/src/main/java/com/example/tubes_01_l/view/EditFragment.java
+++ app/src/main/java/com/example/tubes_01_l/view/EditFragment.java
@@ -46,55 +46,57 @@ public class EditFragment extends Fragment implements View.OnClickListener {
         this.btnEdit=view.findViewById(R.id.btn_edit);
 
         this.btnHapus.setOnClickListener(this);
+        this.btnEdit.setOnClickListener(this);
 
         Bundle b = getArguments();
+
+        this.edTitle.setText("");
         if (b != null) {
             Menu idMenu = b.getParcelable("editMenu");
             Log.d("edit", "onCreateView: " + idMenu.getId());
 
             this.menu = this.sqlite.getContact(idMenu.getId());
-            String title = this.menu.getTitle();
-            Log.d("edit", "onCreateView: sqnya- " + title);
 
-            this.edTitle.setText(this.menu.getTitle());
-            Log.d("edit", "onCreateView: sqnya2- " + title);
+            Log.d("edit", "onCreateView: sqnya- " + menu.getTitle());
+            this.edTitle.setText(menu.getTitle());
+            Log.d("edit", "onCreateView: sqnya2- " + menu.getTitle());
 
-//            this.edTitle.setText(editMenu.getTitle());
-//            this.edDesc.setText(editMenu.getDeskripsi());
-//            this.edTag.setText(editMenu.getTag());
-//
-//            String[] arrBahan = menu.getBahan().split("\n");
-//            String bahan = "";
-//            for (int i = 0; i < arrBahan.length; i++) {
-//                if (i == arrBahan.length - 1) {
-//                    bahan += arrBahan[i];
-//                } else {
-//                    bahan += arrBahan[i] + ", ";
-//                }
-//            }
-//            this.edBahan.setText(bahan);
-//
-//            String[] arrLangkah = menu.getLangkahMasak().split("\n");
-//            String langkah = "";
-//            for (int i = 0; i < arrLangkah.length; i++) {
-//                if (i == arrLangkah.length - 1) {
-//                    langkah += arrLangkah[i];
-//                } else {
-//                    langkah += arrLangkah[i] + ", ";
-//                }
-//            }
-//            this.edLangkah.setText(langkah);
-//
-//            String[] arrResto = menu.getResto().split("\n");
-//            String resto = "";
-//            for (int i = 0; i < arrResto.length; i++) {
-//                if (i == arrResto.length - 1) {
-//                    resto += arrResto[i];
-//                } else {
-//                    resto += arrResto[i] + ", ";
-//                }
-//            }
-//            this.edResto.setText(resto);
+            this.edTitle.setText(menu.getTitle());
+            this.edDesc.setText(menu.getDeskripsi());
+            this.edTag.setText(menu.getTag());
+
+            String[] arrBahan = menu.getBahan().split("\n");
+            String bahan = "";
+            for (int i = 0; i < arrBahan.length; i++) {
+                if (i == arrBahan.length - 1) {
+                    bahan += arrBahan[i];
+                } else {
+                    bahan += arrBahan[i] + ", ";
+                }
+            }
+            this.edBahan.setText(bahan);
+
+            String[] arrLangkah = menu.getLangkahMasak().split("\n");
+            String langkah = "";
+            for (int i = 0; i < arrLangkah.length; i++) {
+                if (i == arrLangkah.length - 1) {
+                    langkah += arrLangkah[i];
+                } else {
+                    langkah += arrLangkah[i] + ", ";
+                }
+            }
+            this.edLangkah.setText(langkah);
+
+            String[] arrResto = menu.getResto().split("\n");
+            String resto = "";
+            for (int i = 0; i < arrResto.length; i++) {
+                if (i == arrResto.length - 1) {
+                    resto += arrResto[i];
+                } else {
+                    resto += arrResto[i] + ", ";
+                }
+            }
+            this.edResto.setText(resto);
         } else {
             Log.d("debug", "onCreateView: Menu Not Found");
         }
@@ -122,23 +124,84 @@ public class EditFragment extends Fragment implements View.OnClickListener {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        Bundle b = getArguments();
+
+        this.edTitle.setText("");
+        if (b != null) {
+            Menu idMenu = b.getParcelable("editMenu");
+            Log.d("edit", "onCreateView: " + idMenu.getId());
+
+            this.menu = this.sqlite.getContact(idMenu.getId());
+
+            Log.d("edit", "onCreateView: sqnya- " + menu.getTitle());
+            this.edTitle.setText(menu.getTitle());
+            Log.d("edit", "onCreateView: sqnya2- " + menu.getTitle());
+
+            this.edTitle.setText(menu.getTitle());
+            this.edDesc.setText(menu.getDeskripsi());
+            this.edTag.setText(menu.getTag());
+
+            String[] arrBahan = menu.getBahan().split("\n");
+            String bahan = "";
+            for (int i = 0; i < arrBahan.length; i++) {
+                if (i == arrBahan.length - 1) {
+                    bahan += arrBahan[i];
+                } else {
+                    bahan += arrBahan[i] + ", ";
+                }
+            }
+            this.edBahan.setText(bahan);
+
+            String[] arrLangkah = menu.getLangkahMasak().split("\n");
+            String langkah = "";
+            for (int i = 0; i < arrLangkah.length; i++) {
+                if (i == arrLangkah.length - 1) {
+                    langkah += arrLangkah[i];
+                } else {
+                    langkah += arrLangkah[i] + ", ";
+                }
+            }
+            this.edLangkah.setText(langkah);
+
+            String[] arrResto = menu.getResto().split("\n");
+            String resto = "";
+            for (int i = 0; i < arrResto.length; i++) {
+                if (i == arrResto.length - 1) {
+                    resto += arrResto[i];
+                } else {
+                    resto += arrResto[i] + ", ";
+                }
+            }
+            this.edResto.setText(resto);
+        } else {
+            Log.d("debug", "onCreateView: Menu Not Found");
+        }
+
+    }
+
+    @Override
     public void onClick(View v) {
         if (v == this.btnEdit) {
-            Menu item = new Menu(0, this.newTitle, this.newDesc, this.newTag, this.newBahan, this.newLangkah, this.newResto);
             this.newTitle = this.edTitle.getText().toString().toLowerCase();
             this.newDesc = this.edDesc.getText().toString();
             this.newTag = this.edTag.getText().toString().replaceAll(" ", "").split(",");
             this.newBahan = this.edBahan.getText().toString().replaceAll(" ", "").split(",");
             this.newLangkah = this.edLangkah.getText().toString().replaceAll(" ", "").split(",");
             this.newResto = this.edResto.getText().toString().replaceAll(" ", "").split(",");
-            this.sqlite.updateContact(item);
-            listener.changePage(2);
-            edTitle.setText(null);
-            edDesc.setText(null);
-            edTag.setText(null);
-            edBahan.setText(null);
-            edLangkah.setText(null);
-            edResto.setText(null);
+            Menu item = new Menu(0, this.newTitle, this.newDesc, this.newTag, this.newBahan, this.newLangkah, this.newResto);
+
+            Log.d("inputnya", "onClick: "+menu.getTitle());
+            //this.sqlite.updateContact(item);
+            //listener.changePage(2);
+//            edTitle.setText(null);
+//            edDesc.setText(null);
+//            edTag.setText(null);
+//            edBahan.setText(null);
+//            edLangkah.setText(null);
+//            edResto.setText(null);
         }
         else if(v==this.btnHapus){
             Log.d("edit", "hapus: "+this.menu.getId());
